1bdb1f0ee5179eb4396742a7022960ca11007e0b
nataliizab <nataliia.zab@gmail.com>
2023-04-14 19:59:43 +0100

    completed all challanges

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 let express = require("express");
 let app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 require("dotenv").config();
+
 app.use(function middleware(req, res, next) {
   const method = req.method;
   const path = req.path;
@@ -21,6 +25,35 @@ app.get("/json", (req, res) => {
   } else {
     res.json({ message: "Hello json" });
   }
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time,
+    });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  let word = req.params.word;
+  res.json({ echo: word });
+});
+
+app.get("/name", (req, res) => {
+  let firstName = req.query.first;
+  let lastName = req.query.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+
+app.post("/name", (req, res) => {
+  let string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
 });
 
 // Normal usage
