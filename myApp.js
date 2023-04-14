50f52338d51a1dba2a361fafad96eea764b34f4c
nataliizab <nataliia.zab@gmail.com>
2023-04-14 19:07:02 +0100

    completed 7 challanges

--- myApp.js
+++ myApp.js
@@ -1,6 +1,31 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello world");
+require("dotenv").config();
+app.use(function middleware(req, res, next) {
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  const mySecret = process.env["MESSAGE_STYLE"];
+  if (mySecret === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
